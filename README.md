c1ed31ae692f6694fe141905ea7265e206737208
CPerezz <c.perezbaro@gmail.com>
2021-06-25 13:16:34 +0200

    tmp

---
+++ README.md
@@ -0,0 +1,5 @@
+# zkevm-testing-vectors
+
+Collection of testing vectors for the ZKVM Proofs. 
+
+The idea at least for now is to arive to a consensus on which is the best format for 
---
+++ contracts/StackPushPop/StackPushPop.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title StackPushPop
+ * @dev Store & retrieve value in a variable
+ */
+contract StackPushPop {
+    function push_pop() public pure {
+        uint256 number = 8;
+    }
+}
