f3ac2ca0d73fcf37059ebce038278efd2dcd78ae
han0110 <tinghan0110@gmail.com>
2021-08-30 17:03:45 +0800

    feat: add gethutil to produce same log structure as debug_trace*

---
+++ gethutil/example/mstore_mload.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm/runtime"
+
+	"gethutil"
+)
+
+func main() {
+	address := common.BytesToAddress([]byte{0xff})
+	asm := gethutil.NewAssembly().MStore(0x40, 0x80).MLoad(0x40)
+	contracts := []gethutil.Contract{{Address: address, Asm: asm}}
+
+	logs, err := gethutil.Trace(address, nil, &runtime.Config{GasLimit: 100}, contracts)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	bytes, err := json.MarshalIndent(logs, "", "  ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	ioutil.WriteFile("./output/mstore_mload.json", bytes, 0644)
+}
