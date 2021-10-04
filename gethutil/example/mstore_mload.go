a12a03d31b8672ec29e197b0da1417ecd6b1c89e
han0110 <tinghan0110@gmail.com>
2021-10-04 19:23:16 +0800

    feat: allow to create contract and set balance

--- gethutil/example/mstore_mload.go
+++ gethutil/example/mstore_mload.go
@@ -14,9 +14,9 @@ import (
 func main() {
 	address := common.BytesToAddress([]byte{0xff})
 	asm := gethutil.NewAssembly().MStore(0x40, 0x80).MLoad(0x40)
-	contracts := []gethutil.Contract{{Address: address, Bytecode: asm.Bytecode}}
+	contracts := []gethutil.Account{{Address: address, Bytecode: asm.Bytecode}}
 
-	logs, err := gethutil.TraceTx(address, nil, &runtime.Config{GasLimit: 100}, contracts)
+	logs, err := gethutil.TraceTx(&address, nil, &runtime.Config{GasLimit: 100}, contracts)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to trace tx, err: %v\n", err)
 	}
